19ef31afdd2824bd52de572c141c983b5c971edf
Lynn <e_lynn_@hotmail.com>
2013-11-29 13:41:16 +0800

    init project

---
+++ app/models/line_item.rb
@@ -0,0 +1,11 @@
+class LineItem < ActiveRecord::Base
+  #attr_accessible :cart_id, :product_id
+  belongs_to :product
+  belongs_to :cart
+  #attr_accessible :product
+  #attr_accessible :cart
+
+  def total_price
+    product.price * quantity
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '19055697a4614948b491626db6583808240ef788a74a810174604565f94a4468aa52212c36425c36f23523255f78bfb7b9f783f6957f5bc367139924de4c312f'
