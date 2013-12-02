08810d5391b2dd4227371309a603193ec559a266
Lynn <e_lynn_@hotmail.com>
2013-12-02 13:31:40 +0800

    update source

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,9 +47,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(store_url) }
+        # format.html { redirect_to(store_url) }
         format.js { @current_item = @line_item}
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        # format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
